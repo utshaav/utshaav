62d1ed13e20dbc89b60c2bbace40171b9aed6cad
utshaav <utshaav583@gmail.com>
2022-02-28 20:04:14 +0545

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Navbar from './Components/Navbar';
+import Introduction from './Components/Introduction';
+import Education from './Components/Education';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Navbar />
+    <Introduction />
+    <Education />
+    {/* <App /> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
