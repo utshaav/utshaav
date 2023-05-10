450fbea12c5d1d31fbfb76a21b57f1e547899f84
utshaav <utshaav583@gmail.com>
2023-05-10 08:31:17 +0545

    Added Experiences

--- src/screens/Introduction/Introduction.tsx
+++ src/screens/Introduction/Introduction.tsx
@@ -4,35 +4,46 @@ import image from "../../assets/images/prog3.gif";
 // import Loader from "../../Components/Basics/Loader";
 // import CodeSnipp from "../../Components/CodeSnipp/CodeSnipp";
 import "./Introduction.scss";
+import Experience from "./Experience";
 // import { setInterval } from "timers";
 
 function Introduction() {
-  
+
   return (
     <>
-    <div className="container">
-    {/* <CodeSnipp /> */}
-    <div className="row " style={{height:'91vh'}}>
-      <div className="intro_text col-md-6 center">
-        <p>Hello 👋 <br/><br/>
-        I am <span className="myname" data-text="Utsav Kuinkel">Utsav Kuinkel</span>. 
-        I am a software engineer. I primarily create dynamic web application using 
-        dotnet and also do react for frontend projects.
-        <br />
-        <br/>
-        {/* <span className="branding">Utsav Kuinkel </span> */}
-        </p>
-        {/* <Loader/> */}
+      <div className="container">
         {/* <CodeSnipp /> */}
-        <p className="mail">Get in touch 👉 <span> <a href="mailto:contact-me@utshaav.com.np">contact-me@utshaav.com.np</a></span></p>
+        <div className="row " style={{ height: '91vh' }}>
+          <div className="intro_text col-md-6 center">
+            <p>Hello 👋 <br /><br />
+              I am <span className="myname" data-text="Utsav Kuinkel">Utsav Kuinkel</span>.
+              I am a software engineer. I primarily create dynamic web application using
+              dotnet and also do react for frontend projects.
+              <br />
+              <br />
+              {/* <span className="branding">Utsav Kuinkel </span> */}
+            </p>
+            {/* <Loader/> */}
+            {/* <CodeSnipp /> */}
+            <p className="mail">Get in touch 👉 <span> <a href="mailto:contact-me@utshaav.com.np">contact-me@utshaav.com.np</a></span></p>
+          </div>
+          <div className="col-md-6 center image" style={{}} >
+            <img className="" src={image} alt="Mero pic lol" />
+          </div>
+        </div>
+        <Experience />
+
       </div>
-      <div className="col-md-6 center image" style={{  }} >
-      <img className="" src={image} alt="Mero pic lol" />
+      {/* <div className="top-sticker">
+        <div className="triangle">
+        </div>
       </div>
-    </div>
-    
-  </div>
-  </>
+
+      <div className="bottom-sticker ">
+        <div className="triangle">
+        </div>
+      </div> */}
+    </>
   )
 }
 
